bae7bb33c0ca10d288c96ebc473a87fe0b971278
ALDL <785313918@qq.com>
2020-06-08 23:58:38 +0800

    first commit

---
+++ docs/precache-manifest.1bb41f2e48b0e2e769ccbf0d3cc74a44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc429624003402696139b4d21d999ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b483e3d734e60c2c3c8",
+    "url": "./static/css/2.27e0d935.chunk.css"
+  },
+  {
+    "revision": "096f7bc4f2b7d78dbb65",
+    "url": "./static/css/main.492fdf3a.chunk.css"
+  },
+  {
+    "revision": "3b483e3d734e60c2c3c8",
+    "url": "./static/js/2.ead7a760.chunk.js"
+  },
+  {
+    "revision": "096f7bc4f2b7d78dbb65",
+    "url": "./static/js/main.eb32b84a.chunk.js"
+  },
+  {
+    "revision": "477e1c4dc829fcb4e93f",
+    "url": "./static/js/runtime~main.cbc78c40.js"
+  },
+  {
+    "revision": "7077510a8bbd1381ef41fcd5073258ee",
+    "url": "./static/media/head.7077510a.png"
+  }
+]);
